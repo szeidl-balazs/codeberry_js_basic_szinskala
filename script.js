af0c3c1ce27ba5183f64d071b797cf86ccb9498a
bszeidl <szeidl.balazs@hotmail.com>
2022-12-18 12:59:11 +0100

    Task completed.

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
 
+    let size = 30;    
+    let positionY = 50;
+    let gap = 5;
+    let r = 255;
+    let g = 79;
+    let b = 120;
+
+    for (row = 0; row < 6; row++) {        
+        
+        let positionX = 125;
+
+        for (column = 0; column < 6; column++) {
+            
+            context.fillStyle = `rgb(${r},${g},${b})`;
+            context.fillRect(positionX, positionY, size, size);
+            r -= 7;
+            positionX += size + gap;
+        }  
+
+        b += 15;  
+        positionY += size + gap;  
+    }
+     
 }
 
 window.addEventListener("load", pageLoaded);
